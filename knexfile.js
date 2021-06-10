6495b8dbf30cddf026eabb805780f4c327b21eb4
korsantoss <korsantoss@gmail.com>
2021-06-10 00:50:58 -0300

    config new db

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@
 module.exports = {
     client: 'mysql2',
     connection: {
-      host: 'sql10.freemysqlhosting.net',
-      database: 'sql10415555',
-      user:     'sql10415555',
-      password: '3niKgKZmuA'
+      host: 'remotemysql.com',
+      database: 'iuzAM1kibf',
+      user:     'iuzAM1kibf',
+      password: 'nzCBN9Uvbm'
     },
     pool: {
       min: 2,
